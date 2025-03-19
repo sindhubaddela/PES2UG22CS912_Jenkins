8dda7c046291ee707e41d57472d30ba260df1506
sindhu <sindhubaddela313@gmail.com>
2025-03-19 19:45:33 +0530

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    echo "Building the C++ program..."
+                    sh 'g++ -o PES2UG22CS912 hello.cpp'  // Compile C++ file
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                    echo "Testing the compiled program..."
+                    sh './PES2UG22CS912-1'  // Execute compiled program
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                script {
+                    echo "Deploying the application (Placeholder step)..."
+                }
+            }
+        }
+    }
+
+    post {
+        failure {
+            echo 'Pipeline failed'
+        }
+    }
+}
